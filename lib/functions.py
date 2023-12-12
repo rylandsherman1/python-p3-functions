a9b502e0f1cd451d392de20a063dc8abee0c7f16
Ryland Sherman <rylandsherman1@gmail.com>
2023-12-12 10:41:56 -0700

    complete assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
+
+
+greet_with_default("Alice")
+greet_with_default()
